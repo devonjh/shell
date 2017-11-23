c38d6299a639faf4c54a570e2fb9350504cd9809
devonjh <devon.honig12@gmail.com>
2017-11-23 12:00:42 -0800

    init final project commit

---
+++ cat.c
@@ -0,0 +1,14 @@
+#include "ucode.c"
+
+int main (int argc, char *argv[]) {
+    int fd;
+    char buf[1024];
+
+    if (argc == 2) {                        //a filename is given.
+
+    }
+
+    else if (argc == 1) {                    //no filename passed in.
+
+    }
+}
